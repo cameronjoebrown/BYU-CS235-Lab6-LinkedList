88947a82422b2ebe01633569fef23a9bd2e93570
Cameron Brown <cameronjoebrown@gmail.com>
2018-11-09 08:47:38 -0800

    initialize

---
+++ LinkedList.cpp
@@ -0,0 +1,9 @@
+//
+//  LinkedList.cpp
+//  Lab6
+//
+//  Created by Cam on 11/6/18.
+//  Copyright © 2018 Cameron Brown. All rights reserved.
+//
+
+#include "LinkedList.h"
---
+++ LinkedList.h
@@ -0,0 +1,25 @@
+//
+//  LinkedList.h
+//  Lab6
+//
+//  Created by Cam on 11/6/18.
+//  Copyright © 2018 Cameron Brown. All rights reserved.
+//
+
+#ifndef LinkedList_h
+#define LinkedList_h
+
+#include <stdio.h>
+#include "LinkedListInterface.h"
+
+class LinkedList : public LinkedListInterace {
+public:
+    
+protected:
+    
+private:
+    
+    
+};
+
+#endif /* LinkedList_h */
