a38171479069b6f01c73134470862ad636a10edb
Cameron Joseph Brown <cameronjoebrown@gmail.com>
2018-11-13 10:24:54 -0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -2,7 +2,7 @@
 //  LinkedList.h
 //  Lab6
 //
-//  Created by Cam on 11/6/18.
+//  Created by Cam on 11/12/18.
 //  Copyright © 2018 Cameron Brown. All rights reserved.
 //
 
@@ -11,15 +11,42 @@
 
 #include <stdio.h>
 #include "LinkedListInterface.h"
+//#include <stdexcept>
 
-class LinkedList : public LinkedListInterace {
+using namespace std;
+
+template<typename T>
+class LinkedList : public LinkedListInterface <T> {
 public:
-    
+    LinkedList() {
+        head = nullptr;
+        tail = nullptr;
+        listSize = 0;
+    }
+    ~LinkedList() {
+        clear();
+    }
+    void insertHead(T value) {
+        
+    }
+    void insertTail(T value);
+    void insertAfter(T value, T insertionNode);
+    void remove(T value);
+    void clear();
+    T at(int index);
+    int size();
+    string toString();
 protected:
     
+    
 private:
-    
-    
+    struct Node {
+        T value;
+        Node* next;
+    };
+    Node* head;
+    Node* tail;
+    int listSize;
 };
 
 #endif /* LinkedList_h */
